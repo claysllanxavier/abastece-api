13b65eac1064e447bd59ba70517021be2c76d8e8
Claysllan Xavier <claysllan-xavier@hotmail.com>
2018-11-13 19:38:38 -0300

    :construction: Inicia Crud de postos de gasolina Cria o controller Cria o model Cria a migration

---
+++ app/Controllers/Http/GasStationController.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with gasstations
+ */
+class GasStationController {
+  /**
+   * Show a list of all gasstations.
+   * GET gasstations
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new gasstation.
+   * GET gasstations/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new gasstation.
+   * POST gasstations
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single gasstation.
+   * GET gasstations/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing gasstation.
+   * GET gasstations/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update gasstation details.
+   * PUT or PATCH gasstations/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a gasstation with id.
+   * DELETE gasstations/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = GasStationController
---
+++ database/migrations/1542065915340_gas_station_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class GasStationSchema extends Schema {
+  up () {
+    this.create('gas_stations', (table) => {
+      table.increments()
+      table.string('name',140).notNullable()
+      table.string('address').notNullable()
+      table.decimal('latitude', 9, 6).notNullable()
+      table.decimal('longitude', 9, 6).notNullable()
+      table.string('phone',15)
+      table.integer('type_id').unsigned().index()
+      table.foreign('type_id').references('id').inTable('types').onDelete('SET NULL');
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('gas_stations')
+  }
+}
+
+module.exports = GasStationSchema
