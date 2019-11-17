ac51d916f1477a45a844233a6a6f034bbfef3a6a
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-11-17 20:45:34 -0300

    :rocket: Configura arquivo de start do servidor

--- server.js
+++ server.js
@@ -18,18 +18,8 @@
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
-const path = require('path')
-const https = require('https')
-const fs = require('fs')
-
-// Certificate
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './server.key')),
-  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
-}
-
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer(handler => https.createServer(options, handler))
+  .fireHttpServer()
   .catch(console.error)
